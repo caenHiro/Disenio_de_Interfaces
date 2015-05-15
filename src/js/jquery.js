f453c139690f73753eb66da65036801d9bb5a19a
jpachecov <la.parte.imaginaria@gmail.com>
2015-05-15 03:34:52 -0500

    aaa

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     var url = document.location.href,
         params = url.split('?')[1].split('&'),
-    document.getElementById('info_hojita').innerHTML = decodeURIComponent(params);
+    	document.getElementById('info_hojita').innerHTML = decodeURIComponent(params);
 }
